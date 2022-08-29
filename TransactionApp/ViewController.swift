0318f6f623f927ffe93234987e3443b59686f7ca
Stan <toju2031@gmail.com>
2022-08-29 13:32:15 +0100

    project initialization with .gitignore file

---
+++ TransactionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TransactionApp
+//
+//  Created by Toju on 8/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
