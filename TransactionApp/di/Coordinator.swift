2cf8f69fc70c945526efb80bc654a712ae96da85
Stan <toju2031@gmail.com>
2022-08-31 05:41:09 +0100

    Removed storyboard for programatic UI, setup coordinator pattern for navigation

---
+++ TransactionApp/di/Coordinator.swift
@@ -0,0 +1,15 @@
+//
+//  Coordinator.swift
+//  TransactionApp
+//
+//  Created by Toju on 8/31/22.
+//
+
+import Foundation
+import UIKit
+
+protocol Coordinator {
+    func launchController() -> UIViewController
+}
+
+
---
+++ TransactionApp/di/MainCoordinator.swift
@@ -0,0 +1,19 @@
+//
+//  MainCoordinator.swift
+//  TransactionApp
+//
+//  Created by Toju on 8/31/22.
+//
+
+import UIKit
+
+
+class MainCoordinator: Coordinator {
+    
+    func launchController() -> UIViewController{
+        let vc = ViewController()
+        return vc
+    }
+    
+    
+}
